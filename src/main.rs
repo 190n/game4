968e46ab994afb036b815aff66d2065665882d3f
Ben Grant <benjamin.j.grant@gmail.com>
2023-05-03 12:41:46 -0700

    notan

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,33 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::{Color, PixelFormatEnum};
-use std::time::Duration;
+use entity::Entity;
+use notan::{draw::*, prelude::*};
 
 mod entity;
 
-pub fn main() {
-	let sdl_context = sdl2::init().unwrap();
-	let video_subsystem = sdl_context.video().unwrap();
+#[derive(AppState)]
+struct State {
+	entities: Vec<Entity>,
+}
 
-	let window = video_subsystem
-		.window("rust-sdl2 demo", 800, 600)
-		.position_centered()
-		.build()
-		.unwrap();
-
-	let mut canvas = window.into_canvas().build().unwrap();
-
-	let mut ents = vec![entity::Looper::new()];
-
-	canvas.clear();
-	canvas.present();
-	let mut event_pump = sdl_context.event_pump().unwrap();
-	let mut i = 0;
-	'running: loop {
-		i = (i + 1) % 255;
-		canvas.set_draw_color(Color::BLACK);
-		canvas.clear();
-
-		for e in &mut ents {
-			e.tick(1.0 / 60.0);
-			e.draw(&mut canvas);
-		}
-
-		for event in event_pump.poll_iter() {
-			match event {
-				Event::Quit { .. }
-				| Event::KeyDown {
-					keycode: Some(Keycode::Escape),
-					..
-				} => break 'running,
-				_ => {},
-			}
-		}
-		// The rest of the game loop goes here...
-
-		canvas.present();
-		std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+fn initial_state() -> State {
+	State {
+		entities: vec![entity::Looper::new()],
 	}
+}
+
+#[notan_main]
+fn main() -> Result<(), String> {
+	notan::init_with(initial_state)
+		.add_config(DrawConfig)
+		.draw(draw)
+		.build()
+}
+
+fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
+	let mut canvas = gfx.create_draw();
+	canvas.clear(Color::BLACK);
+	for e in &mut state.entities {
+		e.tick(app.timer.delta_f32());
+		e.draw(&mut canvas);
+	}
+	gfx.render(&canvas);
 }
