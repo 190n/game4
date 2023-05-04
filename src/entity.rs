320a0e9521559e8627cefb89ee622d5800e5c654
Ben Grant <benjamin.j.grant@gmail.com>
2023-05-04 14:45:31 -0700

    Resizeable window + FPS counter

--- src/entity.rs
+++ src/entity.rs
@@ -1,4 +1,5 @@
 use notan::draw::*;
+use std::collections::VecDeque;
 
 pub trait Behavior {
 	fn tick(&mut self, ent: &mut EntityCommon, dt: f32) -> ();
@@ -6,11 +7,11 @@ pub trait Behavior {
 }
 
 pub struct EntityCommon {
-	pub x: f64,
-	pub y: f64,
+	pub x: f32,
+	pub y: f32,
 }
 
-pub struct Entity(EntityCommon, Box<dyn Behavior>);
+pub struct Entity(pub EntityCommon, pub Box<dyn Behavior>);
 
 impl Entity {
 	pub fn tick(&mut self, dt: f32) -> () {
@@ -36,11 +37,46 @@ impl Behavior for Looper {
 	fn tick(&mut self, ent: &mut EntityCommon, dt: f32) -> () {
 		self.t += dt;
 		self.t %= 2.0 * std::f32::consts::PI;
-		ent.x = f64::cos(self.t as f64) * 100.0 + 400.0;
-		ent.y = f64::sin(self.t as f64) * 100.0 + 300.0;
+		ent.x = f32::cos(self.t) * 100.0 + 200.0;
+		ent.y = f32::sin(self.t) * 100.0 + 200.0;
 	}
 
-	fn draw(&self, ent: &EntityCommon, canvas: &mut Draw) -> () {
-		canvas.rect((ent.x as f32 - 20.0, ent.y as f32 - 20.0), (40.0, 40.0));
+	fn draw(&self, _ent: &EntityCommon, canvas: &mut Draw) -> () {
+		canvas.rect((-20.0, -20.0), (40.0, 40.0));
 	}
+}
+
+pub struct Fps {
+	dts: VecDeque<f32>,
+	counter: u32,
+}
+
+impl Fps {
+	pub fn new() -> Entity {
+		Entity(
+			EntityCommon { x: 0.0, y: 0.0 },
+			Box::new(Fps {
+				dts: VecDeque::with_capacity(60),
+				counter: 0,
+			}),
+		)
+	}
+}
+
+impl Behavior for Fps {
+	fn tick(&mut self, _ent: &mut EntityCommon, dt: f32) -> () {
+		while self.dts.len() >= 60 {
+			self.dts.pop_front();
+		}
+		self.dts.push_back(dt);
+		self.counter += 1;
+		self.counter %= 60;
+		if self.counter == 0 {
+			let frames = self.dts.len();
+			let seconds = self.dts.iter().sum::<f32>();
+			println!("{:>8.2} fps", frames as f32 / seconds);
+		}
+	}
+
+	fn draw(&self, _ent: &EntityCommon, _canvas: &mut Draw) -> () {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use entity::Entity;
-use notan::{draw::*, prelude::*};
+use notan::{
+	draw::*,
+	math::{Mat3, Vec2},
+	prelude::*,
+};
 
 mod entity;
 
@@ -10,7 +14,7 @@ struct State {
 
 fn initial_state() -> State {
 	State {
-		entities: vec![entity::Looper::new()],
+		entities: vec![entity::Looper::new(), entity::Fps::new()],
 	}
 }
 
@@ -18,14 +22,34 @@ fn initial_state() -> State {
 fn main() -> Result<(), String> {
 	notan::init_with(initial_state)
 		.add_config(DrawConfig)
+		.add_config(
+			notan::app::WindowConfig::new()
+				.vsync(true)
+				.title("game4")
+				.size(400, 400)
+				.resizable(true)
+				.high_dpi(true),
+		)
 		.draw(draw)
+		.initialize(initialize)
 		.build()
+}
+
+fn initialize(app: &mut App) {
+	app.window().set_size(400, 400);
 }
 
 fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
 	let mut canvas = gfx.create_draw();
+	let (w, h) = gfx.size();
 	canvas.clear(Color::BLACK);
+	canvas
+		.rect((5.0, 5.0), (w as f32 - 10.0, h as f32 - 10.0))
+		.color(Color::RED);
 	for e in &mut state.entities {
+		canvas.transform().clear();
+		let trans = Mat3::from_translation(Vec2::new(e.0.x, e.0.y));
+		canvas.transform().push(trans);
 		e.tick(app.timer.delta_f32());
 		e.draw(&mut canvas);
 	}
